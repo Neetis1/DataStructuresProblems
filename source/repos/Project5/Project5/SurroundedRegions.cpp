6d35a18a2a50ee1b9172a5b5478a0d43325eb423
Neeti Sharma <sharma1992nee@gmail.com>
2020-07-13 21:01:28 +0530

    Surrounded regions

---
+++ source/repos/Project5/Project5/SurroundedRegions.cpp
@@ -0,0 +1,57 @@
+/*
+Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+Example:
+
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+Explanation:
+
+Surrounded regions shouldn’t be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+*/
+
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        if(m <= 2)
+            return;
+        int n = board[0].size();
+        if(n <=2)
+            return;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i== 0|| j==0 || i == m-1 || j == n-1 && board[i][j] == 'O')
+                    dfs(board,i,j,m,n);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                if(board[i][j] == 'A')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+    void dfs(vector<vector<char>>& board,int i,int j,int m,int n){
+        if(i >= 0 && i < m && j >= 0 && j < n && board[i][j] == 'O')
+        {
+            board[i][j] = 'A';
+            dfs(board,i+1,j,m,n);
+            dfs(board,i,j+1,m,n);
+            dfs(board,i-1,j,m,n);
+            dfs(board,i,j-1,m,n);
+        }
+    }
+};
